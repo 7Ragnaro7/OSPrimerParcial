77660bd94ced1697d2b57f5b360262dd69edb5ef
vagrant <vagrant@vagrantcontainers.example.com>
2021-09-24 04:22:27 +0000

    Agregar procesos al script de python

--- testRequest.py
+++ testRequest.py
@@ -1,8 +1,29 @@
 import requests
+import multiprocessing
+from os import getpid
+import sys
 
-n = 5
-web_url = "http://localhost:9090"
+def make_request():
+    response = requests.get(web_url)
+    print(getpid(), response.headers)
+
+
+if len(sys.argv) != 3:
+    print("Usage: testRequest.py [amount of processes] [port]")
+    sys.exit()
+
+
+web_url = "http://localhost:" + sys.argv[2]
+n = int(sys.argv[1])
+pArr = []
 
 for i in range(n):
-    response = requests.get(web_url)
-    print(response.headers)
+    pArr.append(multiprocessing.Process(target=make_request))
+    pArr[i].start()
+
+for i in range(n):
+    pArr[i].join()
+
+print("All done.")
+
+
